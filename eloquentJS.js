2bbacb49311202c245b12a924d93a7b24d3bfd29
zeitgeist7 <zeitgeist7@gmail.com>
2013-09-08 02:02:31 +0200

    asd

--- eloquentJS.js
+++ eloquentJS.js
@@ -1,9 +1,25 @@
 var anEmail = "Dear nephew,\n\n" + "Your mother told me you have taken up skydiving. Is this true? You watch yourself, young man! Remember what happened to my husband? And that was only from the second floor!\n\n" + "Anyway, things are very exciting here. I have spent all week trying to get the attention of Mr. Drake, the nice gentleman who moved in next door, but I think he is afraid of cats. Or allergic to them? I am going to try putting Fat Igor on his shoulder next time I see him, very curious what will happen.\n\n" + "Also, the scam I told you about is going better than expected. I have already gotten back five 'payments', and only one complaint. It is starting to make me feel a bit bad though. And you are right that it is probably illegal in some way.\n\n" + "(... etc ...)\n\n" + "Much love, Aunt Emily\n\n" + "died 27/04/2006: Black Leclère\n\n" + "born 05/04/2006 (mother Lady Penelope): Red Lion, Doctor Hobbles the 3rd, Little Iroquois";
 
+var names = ["Huey", "Dewey", "Loui"];
+for (var name in names)
+  console.log(names[name]);
 
-// Exercise 4.8
 
-// Exercise 4.9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
